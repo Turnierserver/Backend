8935b07e1fc1791bd5fd6aff9299f5dc2a75236f
Dominic <meiserdo@web.de>
2017-02-11 10:45:29 +0100

    first compilable program

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <QCommandLineOption>
+#include <QCommandLineParser>
+#include <QCoreApplication>
+
+int main(int argc, char **argv)
+{
+	QCoreApplication app(argc, argv);
+	
+	return app.exec();
+}
